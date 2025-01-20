b9b4a6736de62bebdc1f5b4fef446656c38d9380
Rogelio Gamez <gahr61@gmail.com>
2025-01-20 11:43:22 -0600

    feat: countries endpoint

--- app/Http/Controllers/CountriesController.php
+++ app/Http/Controllers/CountriesController.php
@@ -4,7 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Countries;
+use App\Models\States;
+
 class CountriesController extends Controller
 {
-    //
+    public function countries(){
+        $countries = Countries::select('id as value', 'name as label')->get();
+
+        return response()->json(['success' => true, 'data' => $countries]);
+    }
+
+    public function statesByCountry($id){
+        $states = States::select('id as value', 'name as label')
+                        ->where('countries_id', $id)
+                        ->get();
+
+        return response()->json(['success' => true, 'data' => $states]);
+    }
 }
